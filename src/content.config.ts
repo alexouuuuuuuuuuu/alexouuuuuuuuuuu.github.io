6c5c64b3a0b3bb6fd5ba8c4868a6eb6c19ec3145
alexouuuuuuuuuuu <alexis.maugain@etu.unilim.fr>
2025-03-19 11:30:39 +0100

    add

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { glob } from 'astro/loaders';
+import { glob,file } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
@@ -12,7 +12,36 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		auteur: z.string().optional()
 	}),
 });
 
-export const collections = { blog };
+
+
+const pizzas = defineCollection({
+	loader: file("src/content/data/pizzas.json"),
+	schema: z.object({
+	  id: z.number(),
+	  pizzaName: z.string(),
+	  ingredients: z.array(z.string()),
+	}),
+  });
+
+
+  const works = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		auteur: z.string().optional()
+	}),
+});
+
+  
+  export const collections = { blog, pizzas, works};
---
+++ src/content/blog/mon-last-article.md
@@ -0,0 +1,6 @@
+---
+title: 'Last post'
+description: 'Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.'
+pubDate: 'Jun 19 2024'
+heroImage: '/blog-placeholder-1.jpg'
+---
