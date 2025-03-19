11c27a021dae6562739dc5d8893fda4785666843
alexouuuuuuuuuuu <alexis.maugain@etu.unilim.fr>
2025-03-19 12:34:16 +0100

    add

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob,file } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
+import { date } from 'astro:schema';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -18,30 +19,32 @@ const blog = defineCollection({
 
 
 
-const pizzas = defineCollection({
-	loader: file("src/content/data/pizzas.json"),
+const works = defineCollection({
+	loader: file("src/content/data/works.json"),
 	schema: z.object({
 	  id: z.number(),
-	  pizzaName: z.string(),
-	  ingredients: z.array(z.string()),
+	  workName: z.string(),
+	  competences: z.array(z.string()),
+	  photo: z.string(),
+	  date: z.string()
 	}),
   });
 
 
-  const works = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		auteur: z.string().optional()
-	}),
-});
+//   const works = defineCollection({
+// 	// Load Markdown and MDX files in the `src/content/blog/` directory.
+// 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+// 	// Type-check frontmatter using a schema
+// 	schema: z.object({
+// 		title: z.string(),
+// 		description: z.string(),
+// 		// Transform string to Date object
+// 		pubDate: z.coerce.date(),
+// 		updatedDate: z.coerce.date().optional(),
+// 		heroImage: z.string().optional(),
+// 		auteur: z.string().optional()
+// 	}),
+// });
 
   
-  export const collections = { blog, pizzas, works};
+  export const collections = { blog, works};
