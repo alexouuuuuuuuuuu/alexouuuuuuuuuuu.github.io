c5b7152eede7cce1cfb81688ef773d424166c7d6
alexouuuuuuuuuuu <alexis.maugain@etu.unilim.fr>
2025-04-01 12:00:25 +0200

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://alexouuuuuuuuuuu.github.io',
+
   integrations: [mdx(), sitemap()],
 
   vite: {
